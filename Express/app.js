04e5022ec392c92c66ca1435923c457e95e36991
VegaCristian-FS <cavega@student.fullsail.edu>
2022-02-21 20:04:31 -0500

    Assignment #3 submitted

--- Express/app.js
+++ Express/app.js
@@ -31,6 +31,52 @@ router.get('/portfolio', (req, res) =>{
     })
 })
 
+router.get('/contacts', (req, res) =>{
+    res.render('contacts', {
+        pageName: 'contacts'
+    })
+})
+
+//Validate form
+router.post('/contacts', (req, res) => {
+    const errors = []
+    console.log(req.body)
+
+    if (req.body.fname.length === 0) {
+        errors.push('First name cannot be blank')
+    }
+    if (req.body.lname.length === 0) {
+        errors.push('Last name cannot be blank')
+    }
+    if (req.body.address.length === 0) {
+        errors.push('Address cannot be blank')
+    }
+    if (req.body.city.length === 0) {
+        errors.push('City cannot be blank')
+    }
+    if (req.body.state.length === 0) {
+        errors.push('State cannot be blank')
+    }
+    if (req.body.zip.length === 0) {
+        errors.push('zip cannot be blank')
+    }
+    if (req.body.bio.length === 0) {
+        errors.push('Bio cannot be blank')
+    }
+    if (parseInt(req.body.age) < 18) {
+        errors.push('Must be over 18 years old')
+    }
+    if (!req.body.gender) {
+        errors.push('Please choose gender')
+    }
+    if (!req.body['consent']) {
+        errors.push('Must consent before submitting')
+    }
+    res.render('contacts', {
+        pageName: 'contacts', errors
+    })
+})
+
 //Declare static file locations
 app.use(express.static('views'))
 app.use(express.static('public'))
